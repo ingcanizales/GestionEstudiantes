4a7acc5a41ef372bfba169348740cceb6098f454
ingcanizales <ingcanizales2793@gmail.com>
2022-05-10 11:39:38 -0500

    adicion de gestion

---
+++ Gestion/Pages/Estudiantes.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using EstudiantesCore.Dtos;
+using EstudiantesCore.Entidades;
+
+
+namespace GestionEstudiantes.Pages
+{
+    public class EstudiantesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        [HttpPost]
+        public IActionResult OnPostCrearEstudiante(EstudiantesDTO estudiante)
+        {
+
+            try
+            {
+                //_matricula.MatricularEstudiante();
+                return StatusCode(200, "Todo quedo bien");
+            }
+
+            catch (Exception e)
+            {
+                return StatusCode(500, e.Message);
+            }
+
+        }
+    }
+}
